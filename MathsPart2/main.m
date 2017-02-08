639eab65a47036d4ff341efc3d40a673e0c62030
Leo Moon <leomoondev@gmail.com>
2017-02-08 10:10:20 -0800

    Initial Commit

---
+++ MathsPart2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MathsPart2
+//
+//  Created by Hyung Jip Moon on 2017-02-08.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
